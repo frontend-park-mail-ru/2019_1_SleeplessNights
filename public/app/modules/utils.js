4b6da21414514c217cc0fbbd38a8eb24a75ae525
Uimin Maksim <uimin1maksim@yandex.ru>
2019-03-27 00:11:58 +0300

    Avatar path fixed

--- public/app/modules/utils.js
+++ public/app/modules/utils.js
@@ -9,7 +9,7 @@ export const uniqueId = () =>  {
     return '_' + Math.random().toString(36).substr(2, 9);
 };
 
-export const makeAvatarPath = (path) => backendUrl + '/img' + path;
+export const makeAvatarPath = (path) => backendUrl + '/img/' + path;
 
 export const urlencodeFormData = (fd) => {
     let s = '';
