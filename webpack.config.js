258743d1a79caa30f089745937e01953d8226cde
Jahongir <jakha_96-96@mail.ru>
2019-05-12 17:44:15 +0300

    Fixed webpack compiling sass to css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './public/app/app.js',
+    entry: ['./public/app/app.js', './public/assets/scss/main.scss'],
     output: {
-        path: path.resolve(__dirname, './public/assets/js'),
+        path: path.resolve(__dirname, './public/assets/dist'),
         filename: 'bundle.js'
     },
     optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
         minimize: true
     },
     module: {
@@ -26,9 +29,23 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
-                loader: [ "sass-loader", "css-loader", MiniCssExtractPlugin.loader]
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+                loader: 'url-loader?limit=30000&name=[name].[ext]'
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            minimize: true
+        })
+    ],
 };
